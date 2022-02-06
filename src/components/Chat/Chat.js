d843b7752d7d745d1a83a54bb8d13843ada3d91f
한태영 <hantaeyeong@hantaeyeong-ui-MacBookPro.local>
2022-02-06 20:23:25 +0900

    socket io room join

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,32 +4,38 @@ import io from 'socket.io-client'
 
 let socket;
 
-const ENDPOINT = 'http://localhost:5000'
+const ENDPOINT = 'http://44.202.97.232:5000'
 const extraHeaders = {
-  Authorization: "Bearer authorization_token_here"
+ // Authorization: "Bearer authorization_token_here"
 }
 
 export default function Chat() {
-  const [value, setValue] = useState({ name: '', room: '' })
-
+  const [value, setValue] = useState({ name: '', room: '',message:"", messages:[] })
 
   useEffect(() => {
     const { name, room } = queryString.parse(window.location.search)
 
-    socket = io(ENDPOINT, { extraHeaders })
+    //const socket = openSocket('http://localhost:8000', , {transports: ['websocket']});
+    socket = io(ENDPOINT)
 
     setValue({ name, room })
 
     socket.emit('join', { name, room })
-
     return () => {
       socket.emit('disconnect')
       socket.off()
     }
-
   }, [ENDPOINT, window.location.search])
 
+  useEffect(()=>{
+    socket.on('message',(message)=>{
+      setValue({...value, messages:[...value.messages, message]})
+      console.log(value)
+    })
+
+  },[])
+  
   return <div>
     Chat
-  </div>;
+  </div>
 }
